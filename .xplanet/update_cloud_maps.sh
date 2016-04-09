81f998e40f814a97442d9179b9bc97411f4ad929
Paul Cochrane <paul@liekut.de>
2016-04-09 15:04:22 +0200

    [xplanet] add info output when updating cloud maps

--- .xplanet/update_cloud_maps.sh
+++ .xplanet/update_cloud_maps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Fetching new cloud maps"
 wget http://xplanetclouds.com/free/local/clouds_2048.jpg -O ~/.xplanet/cloud_maps/clouds_2048.jpg
+echo "Preparing cloud maps for xplanet"
 xplanet -make_cloud_maps -tmpdir /home/cochrane/.xplanet/cloud_maps/

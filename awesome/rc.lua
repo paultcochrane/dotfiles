16d92b43b3f7127f435c11c024d044c926a0203d
Paul Cochrane <paul@liekut.de>
2017-02-03 12:20:20 +0100

    [awesome] load verion 4.0 config as required
    ... and load the default config if nothing else works

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -4,7 +4,13 @@ version = tonumber(major_version .. "." .. minor_version)
 if version == 3.5 then
     print("Loading awesome 3.5 configuration")
     dofile("/home/cochrane/.config/awesome/rc.lua.3.5")
-else version == 3.4 then
+elseif version == 3.4 then
     print("Loading awesome 3.4 configuration")
     dofile("/home/cochrane/.config/awesome/rc.lua.3.4")
+elseif version == 4.0 then
+    print("Loading awesome 4.0 configuration")
+    dofile("/home/cochrane/.config/awesome/rc.lua.4.0")
+else
+    print("Unknown awesome version; loading default Debian config")
+    dofile("/etc/xdg/awesome/rc.lua")
 end

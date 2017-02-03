f4461c7e52f47b4fb3ab41024a6e0431984cf60f
Paul Cochrane <paul@liekut.de>
2017-02-03 12:15:04 +0100

    [awesome] make subversion and leading 'v' optional in version lookup
    Versions of awesome before 4.x used a 3 point version system (major,
    minor, sub).  The latest version just uses 4.0; it also uses a leading
    'v', which needs to be matched as well.  In Lua, letters are matched by
    %a and the non-greedy form of '*' is '-'.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,5 +1,5 @@
-_, _, major_version, minor_version = string.find(awesome.version, "(%d)%.(%d)%.%d")
 minor_version = tonumber(minor_version)
+_, _, major_version, minor_version = string.find(awesome.version, "%a-(%d)%.(%d)%.-%d-")
 
 if minor_version == 5 then
     print("loading awesome 3.5 configuration")

b794ef3745bddae97ea7607a6b84e752f268bb24
Paul Cochrane <paul@liekut.de>
2017-02-03 12:18:03 +0100

    [awesome] use full version number for config lookup
    We were only using the minor version number.  Now that version 4.0 is
    out, we need to handle the full version number.  This change enables the
    full version number to be used for the appropriate config lookup.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,10 +1,10 @@
-minor_version = tonumber(minor_version)
 _, _, major_version, minor_version = string.find(awesome.version, "%a-(%d)%.(%d)%.-%d-")
+version = tonumber(major_version .. "." .. minor_version)
 
-if minor_version == 5 then
-    print("loading awesome 3.5 configuration")
+if version == 3.5 then
+    print("Loading awesome 3.5 configuration")
     dofile("/home/cochrane/.config/awesome/rc.lua.3.5")
-else
-    print("loading awesome 3.4 configuration")
+else version == 3.4 then
+    print("Loading awesome 3.4 configuration")
     dofile("/home/cochrane/.config/awesome/rc.lua.3.4")
 end

6faf26d572c10836b5fd9717dfc3cff9bd32414b
Paul Cochrane <paul.cochrane@posteo.de>
2020-04-10 15:43:08 +0200

    [awesome] Make config file type obvious from filename
    The config files weren't obviously lua files because their extensions
    weren't `.lua.  This fixes the issue.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -3,19 +3,19 @@ version = tonumber(major_version .. "." .. minor_version)
 
 if version == 3.5 then
     print("Loading awesome 3.5 configuration")
-    dofile("/home/cochrane/.config/awesome/rc.lua.3.5")
+    dofile("/home/cochrane/.config/awesome/rc-3.5.lua")
 elseif version == 3.4 then
     print("Loading awesome 3.4 configuration")
-    dofile("/home/cochrane/.config/awesome/rc.lua.3.4")
+    dofile("/home/cochrane/.config/awesome/rc-3.4.lua")
 elseif version == 4.0 then
     print("Loading awesome 4.0 configuration")
-    dofile("/home/cochrane/.config/awesome/rc.lua.4.0")
+    dofile("/home/cochrane/.config/awesome/rc-4.0.lua")
 elseif version == 4.2 then
     print("Loading awesome 4.2 configuration")
-    dofile("/home/cochrane/.config/awesome/rc.lua.4.2")
+    dofile("/home/cochrane/.config/awesome/rc-4.2.lua")
 elseif version == 4.3 then
     print("Loading awesome 4.3 configuration")
-    dofile("/home/cochrane/.config/awesome/rc.lua.4.3")
+    dofile("/home/cochrane/.config/awesome/rc-4.3.lua")
 else
     print("Unknown awesome version (" .. tostring(version) .. "); loading default Debian config")
     dofile("/etc/xdg/awesome/rc.lua")

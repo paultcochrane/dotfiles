0d6c8f3c7ec0cb5473f7841dd88898bc061d6554
Paul Cochrane <paul.cochrane@posteo.de>
2020-04-10 20:12:48 +0200

    [awesome] Remove mykeyboardlayout from config
    ... because it wasn't useful.

--- awesome/rc-4.3.lua
+++ awesome/rc-4.3.lua
@@ -275,7 +275,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            -- mykeyboardlayout,
             wibox.widget.systray(),
 	    battery_widget,
 	    ram_widget,

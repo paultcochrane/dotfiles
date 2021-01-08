ddb645adf40f5d0cfc7dd7819d5cbd70ed797c3e
Paul Cochrane <paul.cochrane@posteo.de>
2021-01-08 10:48:49 +0100

    [awesome] rename web-devel screen to icysea

--- awesome/rc-4.3.lua
+++ awesome/rc-4.3.lua
@@ -242,7 +242,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "mail", "chat", "web", "devel", "web-devel", "perl", "projects", "oss", "music" }, s, awful.layout.layouts[1])
+    awful.tag({ "mail", "chat", "web", "devel", "icysea", "perl", "projects", "oss", "music" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
